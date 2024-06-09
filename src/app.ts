92b829edaead281c2ab9ac2be320906387d83665
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-06-09 10:29:17 +0200

    Generics VS Union Types

--- src/app.ts
+++ src/app.ts
@@ -75,5 +75,70 @@ function extractAndConvert<T extends object, U extends keyof T>(
 ) {
   return "Value: " + obj[key];
 }
+console.log(extractAndConvert({ name: "Andrea" }, "name")); // Output: Value: Andrea
 
 // Generic Classes
+
+// this not works with reference types, works only with primitive types
+// the only way to make it works with objects is to use the same object
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+/*
+// Object Storage
+const objStorage = new DataStorage<object>();
+const andreaObj = { name: "Andrea" };
+objStorage.addItem(andreaObj);
+objStorage.addItem({ name: "Mark" });
+*/
+
+// Number Storage
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(1);
+numberStorage.addItem(2);
+numberStorage.removeItem(1);
+console.log(numberStorage.getItems()); // Output: [2]
+
+// Generic Utility Types - Partial
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  // Partial<CourseGoal> is a utility type that makes all properties of CourseGoal optional
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+// Readonly
+const names: Readonly<string[]> = ["Andrea", "Mark"];
+// names.push("John"); // this is not valid
+// names.pop(); // this is not valid
+
+// Generic Types vs Union Types
+// Generic types are more flexible and can be used with any type, while union types are more restrictive
+// and can only be used with a specific set of types.
+// In other words, generic types allow you to create reusable components that work with any type, while
+// union types allow you to create components that work with a specific set of types.
